94ba15e56b9b54290f7ce6726f2c1959abbbb4d0
Shishir Sarkar <shishir.sarkar@tavant.com>
2017-08-11 17:03:05 +0530

    added UDF function for generalizing

--- src/main/scala/practise/scala/spark/dao/crud/OrderDataAnalysis.scala
+++ src/main/scala/practise/scala/spark/dao/crud/OrderDataAnalysis.scala
@@ -12,46 +12,36 @@ import org.apache.spark.sql.SQLContext;
 import practise.scala.spark.dao.connection.SparkSqlClass;
 import org.apache.spark.sql.DataFrameReader
 import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.DataFrame;
 
 object OrderDataAnalysis {
- 
- /* def  getDesireDataFrame(sqlContext: SQLContext, dbName: String, dbTable: String):Dataset={
-    val dataFrame =  sqlContext.read.format("jdbc").option("url", dbName)
-                 .option("dbtable", dbTable)
-                 .option("user", "root")
-                 .option("password", "root")
-                 .load();
-     
-   return dataFrame.toDF();
-  }*/
-  
-  def main(orderDataAnalysis: Array[String]){
+
+  def getDesireDataFrame(sqlContext: SQLContext, dbName: String, dbTable: String): DataFrame = {
+    val dataFrame = sqlContext.read.format("jdbc").option("url", dbName)
+      .option("dbtable", dbTable)
+      .option("user", "root")
+      .option("password", "root")
+      .load();
+
+    return dataFrame.toDF();
+  }
+
+  def main(orderDataAnalysis: Array[String]) {
     val sparkSqlClass = new SparkSqlClass();
-    val sparkContext=sparkSqlClass.getSparkContext();
+    val sparkContext = sparkSqlClass.getSparkContext();
     val sqlContext = new SQLContext(sparkContext);
- 
-    val ordersDF = sqlContext.read.format("jdbc").option("url", "jdbc:mysql://localhost:3306/sparkdb")
-                 .option("dbtable", "orders")
-                 .option("user", "root")
-                 .option("password", "root")
-                 .load();
-   
-   
-    val orderItemsDF = sqlContext.read.format("jdbc").option("url", "jdbc:mysql://localhost:3306/sparkdb")
-                 .option("dbtable", "order_items")
-                 .option("user", "root")
-                 .option("password", "root")
-                 .load();
-   
+
+    val ordersDF = getDesireDataFrame(sqlContext, "jdbc:mysql://localhost:3306/sparkdb", "orders");
+
+    val orderItemsDF = getDesireDataFrame(sqlContext, "jdbc:mysql://localhost:3306/sparkdb", "order_items");
+
+    val rightJoinOnOrderAndOrdeItem = ordersDF.join(orderItemsDF, ordersDF("order_id") === orderItemsDF("order_item_order_id"));
+
+    val resultOfRightJoinOrderAndOrdeItem = rightJoinOnOrderAndOrdeItem.select("order_date", "order_status", "order_item_quantity", "order_item_subtotal").orderBy("order_date");
     
-    val rightJoinOnOrderAndOrdeItem = ordersDF.join(orderItemsDF,ordersDF("order_id")===orderItemsDF("order_item_order_id"));
-    
-    val resultOfRightJoinOrderAndOrdeItem = rightJoinOnOrderAndOrdeItem.select("order_date","order_status","order_item_quantity","order_item_subtotal").orderBy("order_date");
-    
-   resultOfRightJoinOrderAndOrdeItem.write.format("com.databricks.spark.csv").save("D:\\tmp\\orders.csv");
-    
-    
-    
-                                  
+    //resultOfRightJoinOrderAndOrdeItem.show();
+
+    resultOfRightJoinOrderAndOrdeItem.write.format("com.databricks.spark.csv").save("D:\\tmp\\orders.csv");
+
   }
 }
