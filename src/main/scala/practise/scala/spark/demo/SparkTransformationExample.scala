11a8f434163dc8a5396149be33006d72b905c68c
Shishir Sarkar <shishir.sarkar@tavant.com>
2018-11-30 09:37:57 +0530

    Added Spark transformation example code

---
+++ src/main/scala/practise/scala/spark/demo/SparkTransformationExample.scala
@@ -0,0 +1,20 @@
+package practise.scala.spark.demo
+
+object SparkTransformationExample {
+  def main(args: Array[String]): Unit = {
+    val sparkConnection = new practise.scala.spark.connection.Connection();
+    val sparkContext = sparkConnection.getSparkContext();
+    
+    println("Flatmap transformation result....");
+    sparkContext.parallelize(List(1,2,3)).flatMap(x=>List(x,x,x)).collect.foreach(print);
+    
+    
+    
+    println("Map transformation result....");
+    sparkContext.parallelize(List(1,2,3)).map(x=>List(x,x,x)).collect.foreach(print);
+    
+    
+
+    
+  }
+}
